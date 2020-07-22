147b3bc1fb4ade7d6797f80450043018c587842e
keerthi <palireddy.keerthi@gmail.com>
2020-07-23 03:30:41 +0530

    added construction class calculated construction cost

--- IntrestCalculationUsingLogger/src/main/java/com/epam/IntrestCalculationUsingLogger/DriverClass.java
+++ IntrestCalculationUsingLogger/src/main/java/com/epam/IntrestCalculationUsingLogger/DriverClass.java
@@ -6,6 +6,6 @@ public class DriverClass {
 	static Logger log = Logger.getLogger(DriverClass.class.getName());
     public static void main( String[] args ) {
     	SI_CI.si_ci_calculation();
-//        Construction.constructionCost();
+        Construction.constructionCost();
     }
 }
